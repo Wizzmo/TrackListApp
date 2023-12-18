8f4c8ed337cd7d4320a23d84e2894b1de845eec6
Maxim Makarov <makarovmaxqa@gmail.com>
2023-12-18 19:40:02 +0500

    Signed the protocol

--- TrackListApp/ViewController.swift
+++ TrackListApp/ViewController.swift
@@ -12,7 +12,18 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
 
-
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
 }
 
