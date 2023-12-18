b87e4b1e4ced5f8801e4c15bb1585add24e1ca51
Maxim Makarov <makarovmaxqa@gmail.com>
2023-12-18 22:12:35 +0500

    Passed the index to the label

--- TrackListApp/ViewController.swift
+++ TrackListApp/ViewController.swift
@@ -22,6 +22,7 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Cell index \(indexPath.description)"
         
         return cell
     }
