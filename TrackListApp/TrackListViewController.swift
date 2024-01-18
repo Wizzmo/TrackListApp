f146d3f1c67cc22ee3c720b46fd5d205e07eebe8
Maxim Makarov <makarovmaxqa@gmail.com>
2024-01-18 21:38:36 +0500

    Change titles

---
+++ TrackListApp/TrackListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TrackListViewController.swift
+//  TrackListApp
+//
+//  Created by Maxim Makarov on 18.01.2024.
+//
+
+import UIKit
+
+final class TrackListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
