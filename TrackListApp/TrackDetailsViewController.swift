0c9c91759e636d727919b22a2cdc821df6b66ea4
Maxim Makarov <makarovmaxqa@gmail.com>
2024-01-24 19:50:20 +0500

    Added TrackDetailsViewController class

---
+++ TrackListApp/TrackDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackListApp
+//
+//  Created by Maxim Makarov on 24.01.2024.
+//
+
+import UIKit
+
+final class TrackDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
